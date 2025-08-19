fc51b4403e9d0b39a8cb1700eeac5dab0f864805
Darthwiings <rohanc1021@gmail.com>
2025-08-19 18:24:23 -0500

    Added filtering by region to plot

--- frontend/app.py
+++ frontend/app.py
@@ -1,4 +1,4 @@
-from dash import Dash, dcc, html
+from dash import Dash, dcc, html, callback, Output, Input
 import plotly.express as px
 import pandas as pd
 
@@ -9,11 +9,11 @@ app = Dash(__name__)
 df = pd.read_csv('../data/cleaned/cleaned_data.csv')
 # Plot dates across months
 df['date'] = pd.to_datetime(df['date'])
-df.set_index('date', inplace=True)
-# Resample
-monthly_sales_df = df.resample('M').sum().reset_index()
 
-fig = px.line(monthly_sales_df, x='date', y='sales',  title='Monthly Sales of Pink Morsels')
+df.set_index('date', inplace=True)
+
+
+
 
 app.layout = html.Div(children=[
     html.H1(
@@ -22,13 +22,24 @@ app.layout = html.Div(children=[
             'textAlign': 'center',
         }
     ),
+    html.Div('Select a region:'),
 
+
+    dcc.RadioItems(options=['north', 'south', 'east', 'west'], value='north',  id='region-control', inline=True),
 
     dcc.Graph(
-        id='example-graph-2',
-        figure=fig
+        id='main-graph',
+        figure={}
     )
 ])
-
+@callback(
+    Output(component_id='main-graph', component_property='figure'),
+    Input(component_id='region-control', component_property='value')
+)
+def update_graph(selected_region):
+    filtered_df = df[df['region'] == selected_region]
+    monthly_sales_df = filtered_df.resample('ME').sum().reset_index()
+    fig = fig = px.line(monthly_sales_df, x='date', y='sales',  title=f'Monthly Sales of Pink Morsels by {selected_region} region')
+    return fig
 if __name__ == '__main__':
     app.run(debug=True)
